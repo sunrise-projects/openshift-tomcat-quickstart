fdec8d21483065ad4c3fbc8d7590c436aab2910c
sunrise-projects <sunrise-projects@users.noreply.github.com>
2015-05-16 02:27:04 -0500

    Update jenkins-build-freestyle.sh

--- jenkins-build-freestyle.sh
+++ jenkins-build-freestyle.sh
@@ -1,5 +1,10 @@
 #/bin/sh
 
+#add this 2 plugin on jenkins
+#https://wiki.jenkins-ci.org/display/JENKINS/GitHub+API+Plugin
+#https://wiki.jenkins-ci.org/display/JENKINS/Github+Plugin
+
+
 #1. check jenkins worker
 #Manage Jenkins -> Manage nodes -> Advance -> # of executors -> 1
 
@@ -13,7 +18,7 @@
 
 export BUILD_NAME=question-app-build
 export REMOTE_HOST=555498a35973ca6a260000d0@portal-mindelements.rhcloud.com
-
+export REMOTE_OPENSHIFT_HOMEDIR=/var/lib/openshift/555498a35973ca6a260000d0/
 rm -rf $OPENSHIFT_DATA_DIR/workspace/$BUILD_NAME/*
 
 mkdir -p $OPENSHIFT_DATA_DIR/workspace/$BUILD_NAME/
@@ -25,23 +30,33 @@ git clone https://github.com/sunrise-projects/question-application.git
 
 cd question-application
 
+#fix env variables
+sed -ig s/portal-librequestion.rhcloud.com/portal-mindelements.rhcloud.com/ question-rest/src/main/webapp/apidocs/index.html
+sed -ig s/localhost:8080/portal-mindelements.rhcloud.com/ question-rest/src/main/webapp/apidocs/index.html
+
+sed -ig s/portal-librequestion.rhcloud.com/portal-mindelements.rhcloud.com/ question-rest/src/main/resources/swagger.properties
+sed -ig s/localhost:8080/portal-mindelements.rhcloud.com/ question-rest/src/main/resources/swagger.properties
+
+sed -ig s/portal-librequestion.rhcloud.com/portal-mindelements.rhcloud.com/ question-web/src/main/java/com/question/web/services/QuestionService.java
+sed -ig s/localhost:8080/portal-mindelements.rhcloud.com/ question-web/src/main/java/com/question/web/services/QuestionService.java
+
+
 # build 
 mvn clean install
 
 # delete war file from remote repo
-ssh -o StrictHostKeyChecking=no -i $OPENSHIFT_DATA_DIR\private.key $REMOTE_HOST 'rm -rf $OPENSHIFT_HOMEDIR/app-root/repo/diy/tomcat/webapps/*.war'
+ssh -o StrictHostKeyChecking=no -i $OPENSHIFT_DATA_DIR\private.key $REMOTE_HOST 'rm -rf $REMOTE_OPENSHIFT_HOMEDIR/app-root/repo/diy/tomcat/webapps/*.war'
 
 # copy war file from maven to remote repo
-scp -o StrictHostKeyChecking=no -i $OPENSHIFT_DATA_DIR\private.key $OPENSHIFT_DATA_DIR/workspace/$BUILD_NAME/*/*/target/*.war $REMOTE_HOST:$OPENSHIFT_HOMEDIR/app-root/repo/diy/tomcat/webapps
+scp -o StrictHostKeyChecking=no -i $OPENSHIFT_DATA_DIR\private.key $OPENSHIFT_DATA_DIR/workspace/$BUILD_NAME/*/*/target/*.war $REMOTE_HOST:$REMOTE_OPENSHIFT_HOMEDIR/app-root/repo/diy/tomcat/webapps
 
 # stop server
-ssh -o StrictHostKeyChecking=no -i $OPENSHIFT_DATA_DIR\private.key $REMOTE_HOST '$OPENSHIFT_HOMEDIR/app-root/repo/.openshift/action_hooks/stop'
+ssh -o StrictHostKeyChecking=no -i $OPENSHIFT_DATA_DIR\private.key $REMOTE_HOST $REMOTE_OPENSHIFT_HOMEDIR/app-root/runtime/repo/.openshift/action_hooks/stop
 
 sleep 30
 
 # sleep server
-ssh -o StrictHostKeyChecking=no -i $OPENSHIFT_DATA_DIR\private.key $REMOTE_HOST '$OPENSHIFT_HOMEDIR/app-root/repo/.openshift/action_hooks/start'
-
+ssh -o StrictHostKeyChecking=no -i $OPENSHIFT_DATA_DIR\private.key $REMOTE_HOST $REMOTE_OPENSHIFT_HOMEDIR/app-root/runtime/repo/.openshift/action_hooks/start
 
 
 
